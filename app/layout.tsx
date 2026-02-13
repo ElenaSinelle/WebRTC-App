5c410f12d7b88f3d26adc48189e9eec070078487
Elena Sinelnikova <elle.sinelle@gmail.com>
2026-02-13 12:01:33 +0300

    fix: fix files naming

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import Header from './components/layout/header/header';
-import Footer from './components/layout/footer/footer';
+import Header from './components/layout/header/Header';
+import Footer from './components/layout/footer/Footer';
 
 export const metadata: Metadata = {
   title: 'WebRTC App',
